5e176bc0c74b98a283b90433e153b9d5700a7153
Dagg <daggintosh@outlook.com>
2019-08-22 12:46:28 -0700

    Play&Pause

--- bot.js
+++ bot.js
@@ -22,6 +22,10 @@ client.on("message", message => {
     if (!message.content.startsWith(prefix)) return
     let argument = message.content.slice(prefix.length).trim().split(/ +/g)
     let command = argument.shift()
+    let author = message.author.id
+    const filter = (reaction, user) => {
+        return [reaction.emoji.name == "⏯"] && user.id == author
+    }
     switch (command) {
         case "add":
             if (!argument[0]) return message.channel.send("No URL provided")
@@ -79,7 +83,6 @@ client.on("message", message => {
             break
 
         case "start":
-            let author = message.author.id
             let dispatch
             let playingembed
             if (alreadyactive == true) return message.channel.send("There's something already playing")
@@ -89,31 +92,31 @@ client.on("message", message => {
             if (!message.member.voiceChannel.joinable) return message.channel.send("I am not able to join this voice channel")
             alreadyactive = true
             message.channel.send("Starting")
-                .then(thismsg => {
+                .then(async thismsg => {
                     message.member.voiceChannel.join()
                         .then(connection => {
                             play0()
                             function play0() {
                                 youtube.getBasicInfo(queue[0])
-                                .then(info => {
-                                    playingembed = {
-                                        embed: {
-                                            title: "Now Playing",
-                                            description: info.title,
-                                            author: {
-                                                name: info.author.name,
-                                                icon_url: info.author.avatar,
-                                                url: info.author.channel_url
-                                            },
-                                            footer: {
-                                                text: `${info.player_response.videoDetails.viewCount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")} viiews`
-                                            },
-                                            color: 0xFF0000,
-                                            image: info.player_response.videoDetails.thumbnail.thumbnails[3]
+                                    .then(info => {
+                                        playingembed = {
+                                            embed: {
+                                                title: "Now Playing",
+                                                description: info.title,
+                                                author: {
+                                                    name: info.author.name,
+                                                    icon_url: info.author.avatar,
+                                                    url: info.author.channel_url
+                                                },
+                                                footer: {
+                                                    text: `${info.player_response.videoDetails.viewCount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")} viiews`
+                                                },
+                                                color: 0xFF0000,
+                                                image: info.player_response.videoDetails.thumbnail.thumbnails[3]
+                                            }
                                         }
-                                    }
-                                    thismsg.edit(playingembed)
-                                })
+                                        thismsg.edit(playingembed)
+                                    })
                                 dispatch = connection.playStream(youtube(queue[0], { highWaterMark: 32000000 }))
                                     .on("end", () => {
                                         if (queue.length > 1) {
@@ -126,8 +129,20 @@ client.on("message", message => {
                                             connection.disconnect()
                                         }
                                     })
+                                thismsg.createReactionCollector(filter, { time: null })
+                                    .on("collect", reaction => {
+                                        reaction.remove(author)
+                                        switch (reaction.emoji.name) {
+                                            case "⏯":
+                                                if (dispatch.paused == false) return dispatch.pause()
+                                                dispatch.resume()
+                                                break
+                                        }
+                                    })
                             }
                         })
+
+                    await thismsg.react("⏯")
                 })
             break
     }
